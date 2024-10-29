c571e54680c95fa48ed34ea70c02ee81d9ad71f5
Jim Barrett <jimbarrett27@gmail.com>
2024-10-29 23:28:56 +0100

    half of 2015 19 done

--- 2015/ex_19.py
+++ 2015/ex_19.py
@@ -1,6 +1,13 @@
 from util.input import get_input
 from collections import defaultdict
 
+test_input = """H => HO
+H => OH
+O => HH
+
+HOHOHO
+"""
+
 def part_1():
     
     puzzle_input = get_input(2015, 19)
@@ -19,7 +26,7 @@ def part_1():
     for source, dests in mappings.items():
 
         source_len = len(source)
-        for i in range(len(medicine) - source_len):
+        for i in range(len(medicine) - source_len + 1):
             if medicine[i: i+source_len] == source:
                 for dest in dests:
                     possible_molecules.add(medicine[:i] + dest + medicine[i+source_len:])
@@ -27,4 +34,26 @@ def part_1():
     return len(possible_molecules)
 
 def part_2():
-    pass
+    
+    puzzle_input = get_input(2015, 19)
+
+    mappings = defaultdict(list)
+    for line in puzzle_input.splitlines():
+
+        if '=>' in line:
+            source, dest = line.split('=>')
+            mappings[source.strip()].append(dest.strip())
+
+        else:
+            medicine = line.strip()
+
+    possible_molecules = set()
+    for source, dests in mappings.items():
+
+        source_len = len(source)
+        for i in range(len(medicine) - source_len + 1):
+            if medicine[i: i+source_len] == source:
+                for dest in dests:
+                    possible_molecules.add(medicine[:i] + dest + medicine[i+source_len:])
+
+    return len(possible_molecules)
