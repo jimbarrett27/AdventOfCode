0b450c14092042aeef23542a89e3c8d67f644d57
Jim Barrett <jimbarrett27@gmail.com>
2024-10-29 21:32:33 +0100

    2015 ex14

--- 2015/ex_14.py
+++ 2015/ex_14.py
@@ -6,53 +6,89 @@ class Reindeer:
 
     name: str
     speed: int
-    endurance: int
-    recovery_period: int
+    stamina: int
+    recovery: int
 
-    position: int = 0
+    pos: int = 0
     time_in_phase: int = 0
-    current_phase: str = 'running'
-
-test_input = """Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
-Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
-"""
+    currently_running: bool = True
 
 def part_1():
     
     puzzle_input = get_input(2015, 14)
 
-    reindeers = []
+    MAX_TIME = 2503
+
+    reindeers: list[Reindeer] = []
     for line in puzzle_input.splitlines():
         words = line.split()
         reindeers.append(Reindeer(
             name=words[0],
             speed=int(words[3]),
-            endurance=int(words[6]),
-            recovery_period=int(words[-2])
+            stamina=int(words[6]),
+            recovery=int(words[-2])
         ))
 
-    for time in range(2503):
+    current_time = 0
+    while current_time < MAX_TIME:
+
         for reindeer in reindeers:
-            if reindeer.current_phase == 'running':
-                if reindeer.time_in_phase == reindeer.endurance:
-                    reindeer.current_phase = 'resting'
-                    reindeer.time_in_phase = 0
-                    continue
-                reindeer.position += reindeer.speed
 
-            elif reindeer.current_phase == 'resting':
-                if reindeer.time_in_phase == reindeer.recovery_period:
-                    reindeer.current_phase = 'running'
-                    reindeer.time_in_phase = 0
-                    continue
+            if reindeer.currently_running and reindeer.time_in_phase == reindeer.stamina:
+                reindeer.currently_running = False
+                reindeer.time_in_phase = 0
 
+            elif not reindeer.currently_running and reindeer.time_in_phase == reindeer.recovery:
+                reindeer.currently_running = True
+                reindeer.time_in_phase = 0
+
+            reindeer.pos += reindeer.currently_running * reindeer.speed
             reindeer.time_in_phase += 1
-        
-    print('\n'.join(map(str,reindeers)))
 
-    winning_reindeer = max(reindeers, key=lambda x: x.position)
+        current_time += 1
 
-    return winning_reindeer.position
+    return max(reindeers, key=lambda x: x.pos).pos
 
 def part_2():
-    pass
+    puzzle_input = get_input(2015, 14)
+
+    MAX_TIME = 2503
+
+    reindeers: list[Reindeer] = []
+    for line in puzzle_input.splitlines():
+        words = line.split()
+        reindeers.append(Reindeer(
+            name=words[0],
+            speed=int(words[3]),
+            stamina=int(words[6]),
+            recovery=int(words[-2])
+        ))
+
+
+    reindeer_to_score = {r.name: 0 for r in reindeers}
+
+    current_time = 0
+    while current_time < MAX_TIME:
+
+        for reindeer in reindeers:
+
+            if reindeer.currently_running and reindeer.time_in_phase == reindeer.stamina:
+                reindeer.currently_running = False
+                reindeer.time_in_phase = 0
+
+            elif not reindeer.currently_running and reindeer.time_in_phase == reindeer.recovery:
+                reindeer.currently_running = True
+                reindeer.time_in_phase = 0
+
+            reindeer.pos += reindeer.currently_running * reindeer.speed
+            reindeer.time_in_phase += 1
+
+        current_time += 1
+
+        furthest_pos = max(reindeers, key=lambda x: x.pos).pos
+
+        for reindeer in reindeers:
+            if reindeer.pos == furthest_pos:
+                reindeer_to_score[reindeer.name] += 1
+
+    return max(reindeer_to_score.values())
