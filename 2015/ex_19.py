ead0def4b13e1b9a02b783c12b2db42a4354fe46
Jim Barrett <jimbarrett27@gmail.com>
2024-11-02 19:07:02 +0100

    2015 e19 p2

--- 2015/ex_19.py
+++ 2015/ex_19.py
@@ -1,13 +1,6 @@
 from util.input import get_input
 from collections import defaultdict
 
-test_input = """H => HO
-H => OH
-O => HH
-
-HOHOHO
-"""
-
 def part_1():
     
     puzzle_input = get_input(2015, 19)
@@ -34,6 +27,10 @@ def part_1():
     return len(possible_molecules)
 
 def part_2():
+    """
+    Extremely unsatisfying solution here, there's no good reaosn this should work
+    but a general solution would be extremely complex to implement
+    """
     
     puzzle_input = get_input(2015, 19)
 
@@ -47,13 +44,20 @@ def part_2():
         else:
             medicine = line.strip()
 
-    possible_molecules = set()
+
+    new_mappings = {}
     for source, dests in mappings.items():
+        for dest in dests:
+            new_mappings[dest] = source
 
-        source_len = len(source)
-        for i in range(len(medicine) - source_len + 1):
-            if medicine[i: i+source_len] == source:
-                for dest in dests:
-                    possible_molecules.add(medicine[:i] + dest + medicine[i+source_len:])
+    mappings = new_mappings
 
-    return len(possible_molecules)
+    molecule = medicine
+    n = 0
+    while molecule != 'e':
+        for product, reactant in sorted(mappings.items(), key=lambda x: len(x[0]), reverse=True):
+            if product in molecule:
+                molecule = molecule.replace(product, reactant, 1)
+                n = n+1
+            
+    return n
