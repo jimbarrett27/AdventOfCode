7637dcb1e471680313fda75671d0d2de51eb6441
Jim Barrett <jimbarrett27@gmail.com>
2025-10-12 18:28:46 +0000

    finish 2016 day 9

--- 2016/ex_9.py
+++ 2016/ex_9.py
@@ -36,5 +36,45 @@ def part_1():
 
     return len(decompressed)
 
+def _decompressed_length(data):
+
+    if '(' not in data:
+        return len(data)
+
+    decompressed_length = 0
+    ind = 0
+    while True:
+        c = data[ind]
+
+        if c == '(':
+            marker_text = ''
+            while c != ')':
+                ind += 1
+                c = data[ind]
+                marker_text += c
+            
+            n_chars, n_repeats = map(int, marker_text[:-1].split('x'))
+            
+            text_to_decompress = ''
+            for _ in range(n_chars):
+                ind += 1
+                text_to_decompress += data[ind]
+            
+            decompressed_length += _decompressed_length(text_to_decompress) * n_repeats
+
+
+        else:
+            decompressed_length += 1    
+
+        ind += 1
+
+        if ind == len(data):
+            break
+
+    return decompressed_length
+
 def part_2():
-    pass
+    
+    data = get_input(2016, 9)
+
+    return _decompressed_length(data)
