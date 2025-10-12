5345a2a2e4183d65f5642f1d1f6334fbb107cc04
Jim Barrett <jimbarrett27@gmail.com>
2025-10-12 18:11:21 +0000

    more manual ex9 pt1 solution

--- 2016/ex_9.py
+++ 2016/ex_9.py
@@ -1,75 +1,40 @@
-from util.input import get_input
 import re
+from util.input import get_input
 
 def part_1():
-    puzzle_input = get_input(2016, 9).strip()
 
-    repetition_command = ''
-    decompressed_string = ''
-    making_command = False
+    data = get_input(2016, 9)
 
-    inp = puzzle_input
-
-    i=0
-    while i < len(inp):
-
-        c = inp[i]
+    decompressed = ''
+    ind = 0
+    while True:
+        c = data[ind]
 
         if c == '(':
-            making_command = True
-        elif making_command and c == ')':
-            making_command = False
-            n_chars, n_reps = map(int, repetition_command.split('x'))
-            decompressed_string += inp[i+1:i+n_chars+1] * n_reps
-            repetition_command = ''
-            i += n_chars
-        elif making_command:
-            repetition_command += c
+            marker_text = ''
+            while c != ')':
+                ind += 1
+                c = data[ind]
+                marker_text += c
+            
+            n_chars, n_repeats = map(int, marker_text[:-1].split('x'))
+            text_to_repeat = ''
+            for _ in range(n_chars):
+                ind += 1
+                text_to_repeat += data[ind]
+            
+            decompressed += text_to_repeat * n_repeats
+
+
         else:
-            decompressed_string += c
+            decompressed += c    
 
-        i += 1
+        ind += 1
 
+        if ind == len(data):
+            break
 
-    return len(decompressed_string)
+    return len(decompressed)
 
 def part_2():
-    
-    puzzle_input = get_input(2016, 9).strip()
-
-    inp = puzzle_input
-
-    i = len(inp) - 1
-
-    def _has_decompression_instruction(s):
-        return re.match(r'\(\d+x\d+\)')
-
-
-
-    def _get_decompressed_length(s):
-
-        if not _has_decompression_instruction(s):
-            return len(s)
-
-        making_command = False
-        repetition_command = ''
-        decompressed_length = 0
-        i=0
-        while i < len(inp):
-
-            c = inp[i]
-
-            if c == '(':
-                making_command = True
-            elif making_command and c == ')':
-                making_command = False
-                n_chars, n_reps = map(int, repetition_command.split('x'))
-                affected_chars = inp[i+1:i+n_chars+1]
-                repetition_command = ''
-                i += n_chars
-            elif making_command:
-                repetition_command += c
-            else:
-                decompressed_length += 1
-
-            i += 1
+    pass
