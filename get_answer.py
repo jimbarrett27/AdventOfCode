ec1c2092bc39335e0a00c0d0c536e3578c81de72
Jim Barrett <jimbarrett27@gmail.com>
2025-12-07 10:06:02 +0100

    time the solutions

--- get_answer.py
+++ get_answer.py
@@ -1,5 +1,10 @@
 import argparse
 import importlib
+from time import perf_counter
+import math
+
+def round_sig(x, sig=3):
+    return round(x, sig - int(math.floor(math.log10(abs(x)))) - 1)
 
 parser = argparse.ArgumentParser(description='Get the solution for a given year and exercise')
 parser.add_argument('year', type=int, help='The AoC year')
@@ -11,8 +16,12 @@ args = parser.parse_args()
 module_name = f'{args.year}.ex_{args.exercise_number}'
 module = importlib.import_module(module_name)
 
+start = perf_counter()
 part_1_answer = getattr(module, 'part_1')()
+part_1_time = round_sig(perf_counter() - start, 3)
+start = perf_counter()
 part_2_answer = getattr(module, 'part_2')()
+part_2_time = round_sig(perf_counter() - start, 3)
 
-print(f'Part 1 answer: {part_1_answer}')
-print(f'Part 2 answer: {part_2_answer}')
+print(f'Part 1 answer: {part_1_answer} time: {part_1_time}')
+print(f'Part 2 answer: {part_2_answer} time: {part_2_time}')
